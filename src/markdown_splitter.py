896d7a1139f9b8a550218f371bb167e03b9116d6
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-10 17:00:04 -0300

    blocks converter involvement == 0 just cving atm

--- src/markdown_splitter.py
+++ src/markdown_splitter.py
@@ -82,3 +82,12 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):  
   matches = re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
   return matches
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes

dc3f6512c5f12a331e439eae066129f2eed7aeff
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-10 12:04:20 -0300

    refactored match case into ifs fixed indentation bug

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,45 +2,74 @@ import unittest
 
 from textnode import (
     TextNode,
-    TextType,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+    text_node_to_html_node,
 )
 
 class TestTextNode(unittest.TestCase):
   def test_eq(self):
-    node = TextNode("This is a text node", TextType.TEXT)
-    node2 = TextNode("This is a text node", TextType.TEXT)
+    node = TextNode("This is a text node", text_type_text)
+    node2 = TextNode("This is a text node", text_type_text)
     self.assertEqual(node, node2)
 
   def test_consts_work(self): 
-    node = TextNode("This is a text node", TextType.BOLD, None)
+    node = TextNode("This is a text node", text_type_bold, None)
     node2 = TextNode("This is a text node", "bold")
     self.assertEqual(node, node2)
 
   def test_eq2(self):
-    node = TextNode("This is a text node", TextType.BOLD, "someurl/with.com")
-    node2 = TextNode("This is a text node", TextType.BOLD, "someurl/with.com")
+    node = TextNode("This is a text node", text_type_bold, "someurl/with.com")
+    node2 = TextNode("This is a text node", text_type_bold, "someurl/with.com")
     self.assertEqual(node, node2)
 
   def test_diff(self):
-    node = TextNode("This is text", TextType.BOLD)
-    node2 = TextNode("This is a text node", TextType.BOLD)
+    node = TextNode("This is text", text_type_bold)
+    node2 = TextNode("This is a text node", text_type_bold)
     self.assertNotEqual(node, node2)
 
   def test_diff2(self):
-    node = TextNode("This is a text node", TextType.BOLD)
-    node2 = TextNode("This is a text node", TextType.ITALIC)
+    node = TextNode("This is a text node", text_type_bold)
+    node2 = TextNode("This is a text node", text_type_italic)
     self.assertNotEqual(node, node2)  
 
   def test_diff3(self):
-    node = TextNode("This is a text node", TextType.BOLD, "someurl/with.com")
-    node2 = TextNode("This is a text node", TextType.BOLD)
+    node = TextNode("This is a text node", text_type_bold, "someurl/with.com")
+    node2 = TextNode("This is a text node", text_type_bold)
     self.assertNotEqual(node, node2)
 
   def test_repr(self):
-        node = TextNode("This is a text node", TextType.TEXT, "https://www.boot.dev")
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
         self.assertEqual(
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )  
+        
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", text_type_text)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_image(self):
+        node = TextNode("This is an image", text_type_image, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props,
+            {"src": "https://www.boot.dev", "alt": "This is an image"},
+        )
+
+    def test_bold(self):
+        node = TextNode("This is bold", text_type_bold)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is bold")
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,13 +1,11 @@
-from enum import Enum
 from leafnode import LeafNode
 
-class TextType(Enum):
-    TEXT = "text"
-    BOLD = "bold"
-    ITALIC = "italic"
-    CODE = "code"
-    LINK = "link"
-    IMAGE = "image"
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
 
 class TextNode:
   def __init__(self, text, text_type, url=None):
@@ -25,22 +23,21 @@ class TextNode:
     return False
 
   def __repr__(self):
-    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    return f"TextNode({self.text}, {self.text_type}, {self.url})"
 
-  def text_node_to_html_node(text_node):
-    match text_node.text_type:
-      case TextType.TEXT:
-        return LeafNode(None, text_node.text)
-      case TextType.BOLD:
-        return LeafNode("b", text_node.text)
-      case TextType.ITALIC:
-        return LeafNode("i", text_node.text)
-      case TextType.CODE:
-        return LeafNode("code", text_node.text)
-      case TextType.LINK:
-        return LeafNode("a", text_node.text, {"href": text_node.url})
-      case TextType.IMAGE:
-        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
-      case _:
-        raise Exception("Invalid text type")
+def text_node_to_html_node(text_node):
+  if text_node.text_type == text_type_text:
+    return LeafNode(None, text_node.text)
+  if text_node.text_type == text_type_bold:
+    return LeafNode("b", text_node.text)
+  if text_node.text_type == text_type_italic:
+    return LeafNode("i", text_node.text)
+  if text_node.text_type == text_type_code:
+    return LeafNode("code", text_node.text)
+  if text_node.text_type == text_type_link:
+    return LeafNode("a", text_node.text, {"href": text_node.url})
+  if text_node.text_type == text_type_image:
+    return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+  raise ValueError(f"Invalid text type: {text_node.text_type}")
+
 
